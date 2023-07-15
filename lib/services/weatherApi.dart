b3a2eeab5da0a02acfced31f0eb075c79532f032
patil-paresh <pareshpatil21.pp@gmail.com>
2023-07-15 15:43:44 +0530

    first commit

--- lib/services/weatherApi.dart
+++ lib/services/weatherApi.dart
@@ -5,8 +5,9 @@ import 'package:weatherapp/model/weatherData.dart';
 
 class WeatherApi {
   Future<WeatherData>? getCurrentWeather() async {
+    String API_KEY = "Enter Your API key";
     var endpoint = Uri.parse(
-        "https://api.weatherapi.com/v1/forecast.json?key=fd93c59e82544823833103447232606&days=7&q=London");
+        "https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&days=7&q=London");
     var response = await http.get(endpoint);
     var body = jsonDecode(response.body);
     print(WeatherData.fromJson(body).cityName);

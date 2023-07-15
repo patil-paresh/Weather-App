d120d701927092d191166ddf29845aece3f9aba6
patil-paresh <pareshpatil21.pp@gmail.com>
2023-07-15 15:49:44 +0530

    ---

--- lib/services/weatherApi.dart
+++ lib/services/weatherApi.dart
@@ -5,7 +5,7 @@ import 'package:weatherapp/model/weatherData.dart';
 
 class WeatherApi {
   Future<WeatherData>? getCurrentWeather() async {
-    String API_KEY = "Enter Your API key";
+    String API_KEY = "Enter Your API_key";
     var endpoint = Uri.parse(
         "https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&days=7&q=London");
     var response = await http.get(endpoint);
